f93f4c8fa7826acf7f067e0116180d34b8d1d090
BJR1984 <brandonreaman@gmail.com>
2022-07-20 14:53:44 -0400

    Update AppliedEnergisticsAddon.java
    AE2 changed this to make dust instead of crystals

--- src/main/java/com/tagnumelite/projecteintegration/addons/AppliedEnergisticsAddon.java
+++ src/main/java/com/tagnumelite/projecteintegration/addons/AppliedEnergisticsAddon.java
@@ -109,7 +109,7 @@ public class AppliedEnergisticsAddon {
             builder.comment("Set defaults conversions for Applied Energistics")
                     .before(CERTUS_QUARTZ_CRYSTAL, 256)
                     .conversion(CERTUS_QUARTZ_CRYSTAL_CHARGED).ingredient(CERTUS_QUARTZ_CRYSTAL).end()
-                    .conversion(FLUIX_CRYSTAL, 2).ingredient(CERTUS_QUARTZ_CRYSTAL_CHARGED).ingredient(Tags.Items.DUSTS_REDSTONE).ingredient(Items.QUARTZ).end()
+                    .conversion(FLUIX_DUST, 2).ingredient(CERTUS_QUARTZ_CRYSTAL_CHARGED).ingredient(Tags.Items.DUSTS_REDSTONE).ingredient(Items.QUARTZ).end()
                     //.conversion(Items.QUARTZ).ingredient(NETHER_QUARTZ_SEED).end()
                     .conversion(FLUIX_CRYSTAL).ingredient(FLUIX_CRYSTAL_SEED).end()
                     .conversion(CERTUS_QUARTZ_CRYSTAL).ingredient(CERTUS_CRYSTAL_SEED).end();
