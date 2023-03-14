91a982f78e7281939c957e5f636d8e4eeec25a4c
Tagan Hoyle <tagnum.elite@gmail.com>
2023-03-14 10:08:32 +0200

    Update support for Ice and Fire

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -5,6 +5,8 @@ The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/),
 and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
 
 ## [Unreleased]
+### Fixed
+- Support for updated [Ice And Fire]
 
 ## [4.5.1]
 ### Fixed
--- src/main/java/com/tagnumelite/projecteintegration/addons/IceAndFireAddon.java
+++ src/main/java/com/tagnumelite/projecteintegration/addons/IceAndFireAddon.java
@@ -24,10 +24,9 @@ package com.tagnumelite.projecteintegration.addons;
 
 import com.github.alexthe666.iceandfire.recipe.DragonForgeRecipe;
 import com.github.alexthe666.iceandfire.recipe.IafRecipeRegistry;
-import com.google.common.collect.ImmutableList;
-import com.tagnumelite.projecteintegration.api.recipe.ACustomRecipeMapper;
+import com.tagnumelite.projecteintegration.api.recipe.ARecipeTypeMapper;
 import com.tagnumelite.projecteintegration.api.recipe.CustomRecipeMapper;
-import net.minecraft.item.ItemStack;
+import net.minecraft.item.crafting.IRecipeType;
 import net.minecraft.item.crafting.Ingredient;
 
 import java.util.Arrays;
@@ -37,35 +36,25 @@ public class IceAndFireAddon {
     public static final String MODID = "iceandfire";
 
     @CustomRecipeMapper(MODID)
-    public static class IAFDragonForgeMapper extends ACustomRecipeMapper<DragonForgeRecipe> {
+    public static class IAFDragonForgeMapper extends ARecipeTypeMapper<DragonForgeRecipe> {
         @Override
         public String getName() {
             return "IceAndFireDragonForgeMapper";
         }
-
         @Override
-        public String getDescription() {
-            return "Recipe mapper for Ice and Fire DragonForge recipes";
-        }
-
-        @Override
-        public List<DragonForgeRecipe> getRecipes() {
-            return ImmutableList.copyOf(IafRecipeRegistry.ALL_FORGE_RECIPES);
+        public boolean canHandle(IRecipeType<?> iRecipeType) {
+            return iRecipeType == IafRecipeRegistry.DRAGON_FORGE_TYPE;
         }
 
         @Override
         protected List<Ingredient> getIngredients(DragonForgeRecipe recipe) {
             return Arrays.asList(recipe.getInput(), recipe.getBlood());
         }
-
-        @Override
-        protected ItemStack getResult(DragonForgeRecipe recipe) {
-            return recipe.getOutput();
-        }
     }
 
     /* TODO: Ice and Fire doesn't like to be run in an dev enviroment.
     @ConversionProvider(MODID)
+    @ObjectHolder(MODID)
     public static class IAFDataGenerator extends AConversionProvider {
         protected static NormalizedSimpleStack bonesTag(String tag) {
             return forgeTag("bones/" + tag);
@@ -79,6 +68,29 @@ public class IceAndFireAddon {
             return tag(MODID + ":" + tag);
         }
 
+        @ObjectHolder("ice_dragon_blood")
+        static Item ICE_DRAGON_BLOOD = null;
+        @ObjectHolder("lightning_dragon_blood")
+        static Item LIGHTNING_DRAGON_BLOOD = null;
+        @ObjectHolder("fire_dragon_blood")
+        static Item FIRE_DRAGON_BLOOD = null;
+        @ObjectHolder("dread_shard")
+        static Item DREAD_SHARD = null;
+        @ObjectHolder("hippogryph_talon")
+        static Item HIPPOGRYPH_TALON = null;
+        @ObjectHolder("hippocampus_fin")
+        static Item HIPPOCAMPUS_FIN = null;
+        @ObjectHolder("shiny_scales")
+        static Item SHINY_SCALES = null;
+        @ObjectHolder("siren_tear")
+        static Item SIREN_TEAR = null;
+        @ObjectHolder("cyclops_eye")
+        static Item CYCLOPS_EYE = null;
+        @ObjectHolder("pixie_dust")
+        static Item PIXIE_DUST = null;
+        @ObjectHolder("pixie_wings")
+        static Item PIXIE_WINGS = null;
+
         @Override
         public void convert(CustomConversionBuilder builder) {
             builder.comment("default conversions for Ice and Fire: Dragons")
@@ -91,18 +103,18 @@ public class IceAndFireAddon {
                     .before(scaleTag("dragon"), 512)
                     .before(bonesTag("dragon"), 156)
                     .before(bonesTag("wither"), 156)
-                    .before(IafItemRegistry.ICE_DRAGON_BLOOD, 256)
-                    .before(IafItemRegistry.LIGHTNING_DRAGON_BLOOD, 256)
-                    .before(IafItemRegistry.FIRE_DRAGON_BLOOD, 256)
-                    .before(IafItemRegistry.DREAD_SHARD, 144)
-                    .before(IafItemRegistry.HIPPOGRYPH_TALON, 128)
-                    .before(IafItemRegistry.HIPPOCAMPUS_FIN, 512)
-                    .before(IafItemRegistry.SHINY_SCALES, 512)
-                    .before(IafItemRegistry.SIREN_TEAR, 768)
-                    .before(IafItemRegistry.CYCLOPS_EYE, 96)
-                    .before(IafItemRegistry.PIXIE_DUST, 1)
-                    .before(IafItemRegistry.PIXIE_WINGS, 1);
+                    .before(ICE_DRAGON_BLOOD, 256)
+                    .before(LIGHTNING_DRAGON_BLOOD, 256)
+                    .before(FIRE_DRAGON_BLOOD, 256)
+                    .before(DREAD_SHARD, 144)
+                    .before(HIPPOGRYPH_TALON, 128)
+                    .before(HIPPOCAMPUS_FIN, 512)
+                    .before(SHINY_SCALES, 512)
+                    .before(SIREN_TEAR, 768)
+                    .before(CYCLOPS_EYE, 96)
+                    .before(PIXIE_DUST, 1)
+                    .before(PIXIE_WINGS, 1);
         }
     }
-    */
+     */
 }
